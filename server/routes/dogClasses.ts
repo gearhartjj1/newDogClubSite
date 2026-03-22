765f8d9df17ccc8f32ac2d588a08b43f2f1cecb3
Jacob Gearhart <jacobjamesgearhart@gmail.com>
2026-03-22 15:24:19 -0400

    limit dog class data to specific session

--- server/routes/dogClasses.ts
+++ server/routes/dogClasses.ts
@@ -6,13 +6,13 @@ const router = express.Router();
 // Get all dog classes
 router.get('/', async (req: Request, res: Response) => {
   try {
-    const query = 'SELECT c.*, COUNT(e.id) AS DogsInClass FROM KCTCSession c INNER JOIN Enrollment e ON c.id = e.SID GROUP BY c.id, c.class ORDER BY c.class';
+    const query = 'SELECT c.*, COUNT(e.id) AS DogsInClass FROM KCTCSession c INNER JOIN Enrollment e ON c.id = e.SID WHERE c.Session = "2024-V" GROUP BY c.id, c.class ORDER BY c.class';
     //const query = 'SELECT * FROM KCTCSession WHERE Session = "2024-V"';
-    // TODO: Execute query using database connection
     const dogClasses = await pool.query(query);
-    //console.log('Dog classes fetched:', dogClasses);
+    console.log('Dog classes fetched:', dogClasses);
     res.json(dogClasses);
   } catch (error) {
+    console.log('Error fetching dog classes: ', error);
     res.status(500).json({ error: 'Failed to fetch dog classes' });
   }
 });
@@ -55,12 +55,6 @@ router.get('/:id', async (req: Request, res: Response) => {
 router.post('/', async (req: Request, res: Response) => {
   console.log('dog class post: ', req.body);
   try {
-    //const { title, date, time, location, description } = req.body;
-    
-    /*if (!title || !date || !time || !location) {
-      return res.status(400).json({ error: 'Missing required fields' });
-    }*/
-
     //TODO: I have the basic query to add rnrollment data
     //-I need to update the form to be more similar to the form in the old site
     //-I need to pass in the class selection id and include it here
@@ -70,13 +64,13 @@ router.post('/', async (req: Request, res: Response) => {
     //SID - session id (class id)? - CORRECT
     //I need to add checks to pull the latest data from the database before performing this
     //I need to verify there are open spots in the class before enrolling
+    //Update class page to not allow enrolling if the max limit of dogs is reached
 
     const maxIdResult = await pool.query('SELECT MAX(ID) AS maxId FROM Enrollment');
     //TODO: this works well, but I should probably add some error handling here in case of failures
     const newIdValue = (maxIdResult[0] as any)[0].maxId + 1;
     console.log("the new id is: ", (maxIdResult[0] as any)[0].maxId);
     const newQuery = `insert into Enrollment values (${newIdValue}, 1, ${req.body.classId}, 'Y', 'Y', 1, '${req.body.dogName}', '${req.body.dogAge}', '${req.body.dogBreed}', 'Y', 'None', 'internet', ${Date.now()})`;
-    console.log('Constructed query: ', newQuery);
     const response = await pool.query(newQuery);
     console.log('Database response: ', response);
     
