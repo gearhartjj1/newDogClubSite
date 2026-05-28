7912ea5de1101eb379f7b13e9072a8459ff6415e
Jacob Gearhart <jacobjamesgearhart@gmail.com>
2026-05-27 22:23:12 -0400

    update ui to have better sizing

--- src/App.tsx
+++ src/App.tsx
@@ -26,31 +26,28 @@ function App() {
       <UserDataProvider>
         <Navigation />
         <div className="app-body">
-        <Sidebar />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/events" element={<Events />} />
-            <Route path="/classes" element={<Classes />} />
-            <Route path="/signup" element={<ClassSignup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/building-usage" element={<BuildingUsage />} />
-            <Route path="/class-prices" element={<ClassPrices />} />
-            <Route path="/contact" element={<ContactUs />} />
-            <Route path="/directions" element={<Directions />} />
-            <Route path="/forms" element={<Forms />} />
-            <Route path="/gift-certificates" element={<GiftCertificates />} />
-            <Route path="/history" element={<History />} />
-            <Route path="/membership" element={<Membership />} />
-            <Route path="/members-only" element={<MembersOnly />} />
-            <Route path="/officers-and-board" element={<OfficersAndBoard />} />
-          </Routes>
-        </main>
+          <Sidebar />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/events" element={<Events />} />
+              <Route path="/classes" element={<Classes />} />
+              <Route path="/signup" element={<ClassSignup />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/building-usage" element={<BuildingUsage />} />
+              <Route path="/class-prices" element={<ClassPrices />} />
+              <Route path="/contact" element={<ContactUs />} />
+              <Route path="/directions" element={<Directions />} />
+              <Route path="/forms" element={<Forms />} />
+              <Route path="/gift-certificates" element={<GiftCertificates />} />
+              <Route path="/history" element={<History />} />
+              <Route path="/membership" element={<Membership />} />
+              <Route path="/members-only" element={<MembersOnly />} />
+              <Route path="/officers-and-board" element={<OfficersAndBoard />} />
+            </Routes>
+          </main>
         </div>
-        <footer className="footer">
-          <p>&copy; 2024 Keystone Canine Training Club. All rights reserved. 🐾</p>
-        </footer>
       </UserDataProvider>
     </Router>
   );
